b0c5fb7439d127e7e2c654f13163f5ad3d876ec2
Avi Agarwal <aviagarwal1212@gmail.com>
2023-11-04 12:21:56 +0000

    respond with 404

--- app/main.py
+++ app/main.py
@@ -7,11 +7,16 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("created server on localhost:4221")
     connection, ret_add = server_socket.accept()
-    print(f"connected to {connection} at {ret_add}")
     with connection:
-        connection.recv(512)
-        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        print("-> responded to connection")
+        request = connection.recv(512).decode()
+        start_line, *headers = request.split("\r\n")
+        method, path, http_version = start_line.split(" ")
+        print(f"received {method} request on {path}")
+        match path:
+            case "/":
+                connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            case _:
+                connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
