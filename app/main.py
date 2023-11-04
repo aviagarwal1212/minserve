dbf8aae9946bda0f49a4df89bda77fd9bd5a764f
Avi Agarwal <aviagarwal1212@gmail.com>
2023-11-04 14:56:19 +0000

    refactor dataclasses

--- app/main.py
+++ app/main.py
@@ -1,26 +1,10 @@
+import asyncio
 from dataclasses import dataclass
 from http import HTTPStatus
-import asyncio
 
 RESPONSE_SEP = "\r\n"
 PORT = 4221
 BUFFER_SIZE = 2048
-
-
-@dataclass
-class HTTPRequest:
-    method: str
-    path: str
-    http_version: str
-    headers: list[str]
-
-    def request_headers(self) -> dict[str, str]:
-        header_dict: dict[str, str] = {}
-        for header in self.headers:
-            if header != "":
-                header_type, header_content = header.split(": ")
-                header_dict[header_type.strip()] = header_content.strip()
-        return header_dict
 
 
 @dataclass
@@ -45,10 +29,48 @@ class HTTPResponse:
         return response_string.encode()
 
 
+@dataclass
+class HTTPRequest:
+    method: str
+    path: str
+    http_version: str
+    headers: list[str]
+
+    async def request_headers(self) -> dict[str, str]:
+        header_dict: dict[str, str] = {}
+        for header in self.headers:
+            if header != "":
+                header_type, header_content = header.split(": ")
+                header_dict[header_type.strip()] = header_content.strip()
+
+        return header_dict
+
+    async def process_response(self) -> HTTPResponse:
+        status = HTTPStatus.NOT_FOUND
+        content: str | None = None
+        match self.path:
+            case "/":
+                status = HTTPStatus.OK
+
+            case path if path.startswith("/echo/"):
+                status = HTTPStatus.OK
+                content = path.split("/echo/")[1]
+
+            case "/user-agent":
+                status = HTTPStatus.OK
+                headers = await self.request_headers()
+                content = headers["User-Agent"]
+
+            case _:
+                ...
+
+        return HTTPResponse(content=content, status=status)
+
+
 async def process_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     request = await process_connection(reader)
     print(f"received {request.method} request on {request.path}")
-    response = await process_response(request)
+    response = await request.process_response()
     response_bytes = await response.return_byte_response()
     writer.write(response_bytes)
     writer.close()
@@ -57,31 +79,9 @@ async def process_client(reader: asyncio.StreamReader, writer: asyncio.StreamWri
 async def process_connection(reader: asyncio.StreamReader) -> HTTPRequest:
     request_bytes = await reader.read(BUFFER_SIZE)
     request = request_bytes.decode()
-    start_line, *headers = request.split("\r\n")
+    start_line, *headers = request.split(RESPONSE_SEP)
     method, path, http_version = start_line.split(" ")
     return HTTPRequest(method, path, http_version, headers)
-
-
-async def process_response(request: HTTPRequest) -> HTTPResponse:
-    status = HTTPStatus.NOT_FOUND
-    content: str | None = None
-    match request.path:
-        case "/":
-            status = HTTPStatus.OK
-
-        case path if path.startswith("/echo/"):
-            status = HTTPStatus.OK
-            content = path.split("/echo/")[1]
-
-        case "/user-agent":
-            status = HTTPStatus.OK
-            headers = request.request_headers()
-            content = headers["User-Agent"]
-
-        case _:
-            ...
-
-    return HTTPResponse(content=content, status=status)
 
 
 async def main():
