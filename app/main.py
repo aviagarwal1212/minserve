1ab4dbb03ef4bc61913bd38f0d04505d9a03684b
Avi Agarwal <aviagarwal1212@gmail.com>
2023-11-04 12:07:20 +0000

    respond with 200

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ import socket
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("created server on localhost:4221")
-    server_socket.accept()
+    connection, ret_add = server_socket.accept()
+    print(f"connected to {connection} at {ret_add}")
+    with connection:
+        connection.recv(512)
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        print("-> responded to connection")
 
 
 if __name__ == "__main__":
