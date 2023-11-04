191e62c9b97f3da2edb2666e83c2436fe645f23c
Avi Agarwal <aviagarwal1212@gmail.com>
2023-11-04 13:59:43 +0000

    parse headers

--- app/main.py
+++ app/main.py
@@ -14,6 +14,14 @@ class Request:
     http_version: str
     headers: list[str]
 
+    def request_headers(self) -> dict[str, str]:
+        header_dict: dict[str, str] = {}
+        for header in self.headers:
+            if header != "":
+                header_type, header_content = header.split(": ")
+                header_dict[header_type.strip()] = header_content.strip()
+        return header_dict
+
 
 @dataclass
 class Response:
@@ -25,15 +33,15 @@ class Response:
     def __post_init__(self):
         if self.content is not None:
             self.content_length = len(self.content)
+            self.content_type = "text/plain"
 
     def return_byte_response(self) -> bytes:
         response_string = f"HTTP/1.1 {self.status}{RESPONSE_SEP}"
         if self.content is not None:
             response_string += f"Content-Type: {self.content_type}{RESPONSE_SEP}"
             response_string += f"Content-Length: {self.content_length}{RESPONSE_SEP*2}"
-            response_string += f"{self.content}{RESPONSE_SEP*2}"
-        else:
-            response_string += f"{RESPONSE_SEP}"
+            response_string += f"{self.content}{RESPONSE_SEP}"
+        response_string += f"{RESPONSE_SEP}"
         return response_string.encode()
 
 
@@ -59,18 +67,23 @@ def process_connection(connection: socket) -> Request:
 def process_response(request: Request) -> Response:
     status = HTTPStatus.NOT_FOUND
     content: str | None = None
-    content_type: str | None = None
     match request.path:
         case "/":
             status = HTTPStatus.OK
+
         case path if path.startswith("/echo/"):
             status = HTTPStatus.OK
             content = path.split("/echo/")[1]
-            content_type = "text/plain"
+
+        case "/user-agent":
+            status = HTTPStatus.OK
+            headers = request.request_headers()
+            content = headers["User-Agent"]
+
         case _:
             ...
 
-    return Response(content=content, content_type=content_type, status=status)
+    return Response(content=content, status=status)
 
 
 if __name__ == "__main__":
